bd5e1fe5e86a1abdf5bc4e2c5e955bef12d731c5
Marc Richter <mr@in-telegence.net>
2014-12-22 14:11:57 +0100

    Reverted stupid idea

--- init.sh
+++ init.sh
@@ -9,16 +9,5 @@ if [ ! -d /var/www/html/piwik ]; then
     chown apache:apache -R piwik
 fi
 
-# Configure Piwik if unconfigured
-if [ ! -e /var/www/html/piwik/config/config.ini.php ]; then
-    cp -f /tmp/default_piwik_config.ini /var/www/html/piwik/config/config.ini.php
-    sed -i'' "s#CHANGE_ME_DBHOST#${DB_HOST}#g" /var/www/html/piwik/config/config.ini.php
-    sed -i'' "s#CHANGE_ME_DBUSER#${DB_USER}#g" /var/www/html/piwik/config/config.ini.php
-    sed -i'' "s#CHANGE_ME_DBPASS#${DB_PASS}#g" /var/www/html/piwik/config/config.ini.php
-    sed -i'' "s#CHANGE_ME_DBNAME#${DB_NAME}#g" /var/www/html/piwik/config/config.ini.php
-    sed -i'' "s#CHANGE_ME_SALT#$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)#g" /var/www/html/piwik/config/config.ini.php
-    sed -i'' "s#CHANGE_ME_TRHOST#${TR_HOST}#g" /var/www/html/piwik/config/config.ini.php
-fi
-
 # Start supervisord in foreground (must be the last action of this script)
 /usr/bin/supervisord -c /etc/supervisord.conf --nodaemon
