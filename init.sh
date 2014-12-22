64d1a5ba2bc42dec829a0cd7b3f5de001601483b
Marc Richter <mr@in-telegence.net>
2014-12-22 14:10:34 +0100

    Added TR_HOST variable for dynamic config

--- init.sh
+++ init.sh
@@ -17,7 +17,7 @@ if [ ! -e /var/www/html/piwik/config/config.ini.php ]; then
     sed -i'' "s#CHANGE_ME_DBPASS#${DB_PASS}#g" /var/www/html/piwik/config/config.ini.php
     sed -i'' "s#CHANGE_ME_DBNAME#${DB_NAME}#g" /var/www/html/piwik/config/config.ini.php
     sed -i'' "s#CHANGE_ME_SALT#$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)#g" /var/www/html/piwik/config/config.ini.php
-    sed -i'' "s#CHANGE_ME_TRHOST#localhost#g" /var/www/html/piwik/config/config.ini.php
+    sed -i'' "s#CHANGE_ME_TRHOST#${TR_HOST}#g" /var/www/html/piwik/config/config.ini.php
 fi
 
 # Start supervisord in foreground (must be the last action of this script)
